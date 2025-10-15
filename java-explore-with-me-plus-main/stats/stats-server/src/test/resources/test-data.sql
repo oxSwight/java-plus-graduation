e16e0f1aab6941f4c5e1d40393fe737900420c5e
IgorZ <igorzomm3r@gmail.com>
2025-10-15 18:31:11 +0200

    delete useless files

--- java-explore-with-me-plus-main/stats/stats-server/src/test/resources/test-data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO hits (app, uri, ip, timestamp)
-VALUES
-    ('event-service', '/events/1', '12.452.0.1', TIMESTAMP '2025-01-01 12:00:00'),
-    ('event-service', '/events/1', '12.452.0.1', TIMESTAMP '2025-01-01 12:30:00'),
-    ('event-service', '/events/1', '12.452.0.1', TIMESTAMP '2025-01-02 10:00:00'),
-    ('event-service', '/events/2', '12.452.0.4', TIMESTAMP '2025-01-01 15:00:00'),
-    ('event-service', '/events/2', '12.452.0.5', TIMESTAMP '2025-01-02 11:00:00'),
-    ('user-service', '/users', '192.168.1.5', TIMESTAMP '2025-01-02 14:00:00');
