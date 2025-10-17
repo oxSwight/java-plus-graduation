a252b9aebfc6a1d9b623a4ea12d8b7871d2eec03
IgorZ <igorzomm3r@gmail.com>
2025-10-17 19:29:48 +0200

    fix bugs

--- core/main-service/src/main/java/ru/practicum/ewm/MainApplication.java
+++ core/main-service/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.practicum.ewm.stats.client.StatsClient;
+import ru.practicum.ewm.stats.client.StatClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {StatsClient.class})
+@EnableFeignClients(clients = {StatClient.class})
 public class MainApplication {
 
     public static void main(String[] args) {
