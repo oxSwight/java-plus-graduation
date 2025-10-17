131e56731d6eb6489e8e1fe2c33c2eb10631040b
IgorZ <igorzomm3r@gmail.com>
2025-10-17 12:33:52 +0200

    updating StatDto

--- java-explore-with-me-plus-main/stats/stats-dto/src/main/java/ru/practicum/explore/dto/StatDto.java
+++ java-explore-with-me-plus-main/stats/stats-dto/src/main/java/ru/practicum/explore/dto/StatDto.java
@@ -1,18 +1,17 @@
 package ru.practicum.explore.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
+@Getter
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
-@Data
-@NoArgsConstructor
 public class StatDto {
-
     String app;
     String uri;
     Long hits;
-
 }
