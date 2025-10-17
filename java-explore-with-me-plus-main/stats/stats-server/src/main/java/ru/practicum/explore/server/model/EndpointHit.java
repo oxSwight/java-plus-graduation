60e20d9bb145124261431be60427769ff8995bc1
IgorZ <igorzomm3r@gmail.com>
2025-10-17 12:49:00 +0200

    update EndpointHit

--- java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/model/EndpointHit.java
+++ java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/model/EndpointHit.java
@@ -2,31 +2,27 @@ package ru.practicum.explore.server.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "hits")
+@Table(name = "hit")
+@ToString
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder(toBuilder = true)
 public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "app", nullable = false, length = 128)
-    private String app;
-
-    @Column(name = "uri", columnDefinition = "TEXT", nullable = false)
-    private String uri;
-
-    @Column(name = "ip", nullable = false)
-    private String ip;
-
-    @Column(name = "timestamp", nullable = false)
-    private LocalDateTime timestamp;
+    @Column(name = "hit_id")
+    Long id;
+    String app;
+    String uri;
+    String ip;
+    @Column(name = "created_date")
+    LocalDateTime createdDate;
 }
