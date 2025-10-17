73f840d7a0501d3720abae50f4dc26eadd3b1921
IgorZ <igorzomm3r@gmail.com>
2025-10-17 12:49:20 +0200

    update tool/Errors

--- java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/tools/ErrorHandler.java
+++ java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/tools/ErrorHandler.java
@@ -1,32 +1,51 @@
 package ru.practicum.explore.server.tools;
 
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleNotFound(final IllegalArgumentException e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidExceptions(MethodArgumentNotValidException e) {
+        String message = e.getMessage();
+        log.debug("Получен статус 400 BAD_REQUEST {}", message, e);
+        return new ErrorResponse(message);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        String message = e.getMessage();
+        log.debug("Получен статус 400 BAD_REQUEST {}", message, e);
+        return new ErrorResponse(message);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleAllExceptions(Exception e) {
+        log.debug("Получен статус 500 INTERNAL_SERVER_ERROR {}", e.getMessage(), e);
+        return new ErrorResponse("Ой у нас чтото сломалось :)");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse handleStartEndDateNotExists(final MissingServletRequestParameterException e) {
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMissingRequestParameterException(final MissingServletRequestParameterException e) {
-        return new ErrorResponse("Забыт обязательный параметр + " + e.getParameterName() + ".");
-    }
-
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleGeneric(final Throwable e) {
-        if (e.getMessage() != null && !e.getMessage().isEmpty()) {
-            return new ErrorResponse(e.getMessage());
-        }
-        return new ErrorResponse("Ошибка.");
+    public ErrorResponse handleValidation(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
     }
 }
--- java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/tools/ErrorResponse.java
+++ java-explore-with-me-plus-main/stats/stats-server/src/main/java/ru/practicum/explore/server/tools/ErrorResponse.java
@@ -1,14 +1,4 @@
 package ru.practicum.explore.server.tools;
 
-import lombok.Getter;
-
-@Getter
-public class ErrorResponse {
-
-    private final String error;
-
-    public ErrorResponse(String error) {
-        this.error = error;
-    }
-
+public record ErrorResponse(String error) {
 }
